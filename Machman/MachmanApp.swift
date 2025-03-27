e1c638460b028f73824e6cc545728b4124d929c5
Gustaf Franzen <gustaffranzen@icloud.com>
2025-03-27 12:26:10 +0100

    Initial Commit

---
+++ Machman/MachmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachmanApp.swift
+//  Machman
+//
+//  Created by Gustaf Franzen on 2025-03-27.
+//
+
+import SwiftUI
+
+@main
+struct MachmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MachmanTests/MachmanTests.swift
@@ -0,0 +1,17 @@
+//
+//  MachmanTests.swift
+//  MachmanTests
+//
+//  Created by Gustaf Franzen on 2025-03-27.
+//
+
+import Testing
+@testable import Machman
+
+struct MachmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
